ad71b1f5539b018b6dc40d80c8af91c031226220
AK-uni-git <55245797+AK-uni-git@users.noreply.github.com>
2020-01-13 13:22:13 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MenuAppBar from './Components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <MenuAppBar/>
+    </div>
+  );
+}
+
+export default App;
